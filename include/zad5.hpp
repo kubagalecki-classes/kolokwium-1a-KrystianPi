fddce5356f75511c33af2937f4d4daf0c3e614a8
KrystianPi <38494173+KrystianPi@users.noreply.github.com>
2021-01-14 14:25:06 +0100

    Update zad5.hpp

--- include/zad5.hpp
+++ include/zad5.hpp
@@ -7,6 +7,6 @@
 #include <vector>
 template<typename T, typename R>
 void sortujTagliatelle(T t, R r) {
-    std::sort(t, r, []( Tagliatelle a, Tagliatelle b) {return a.ileMaki() > b.ileMaki(); });
+    std::sort(t, r, []( Tagliatelle a, Tagliatelle b) {return a.ileMaki(1) > b.ileMaki(1); });
 }
 // tutaj funkcja sortujTagliatelle
