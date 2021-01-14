fdf2eb5e25f56a7a77f4bdeada3ba4841632d1e5
KrystianPi <38494173+KrystianPi@users.noreply.github.com>
2021-01-14 13:59:52 +0100

    Update zad2.hpp

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -5,5 +5,19 @@
 #include <type_traits>
 
 // tutaj klasa Penne
-
+class Penne : public Makaron {
+public:
+    double ileMaki(unsigned P) const override { return double(P); }
+};
 // tutaj definicja metody gotujMakaron
+ Makaron* Makaron::gotujMakaron(const std::string& s) {
+     Makaron* a;
+     if (s.front() == s.back()) {
+         a = new Tagliatelle(3.14, 0.42, 0.1);
+     }
+     else {
+         a = new Penne;
+     }
+ 
+     return a;
+ }
