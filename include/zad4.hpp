853f72f852728c06170b5a94b64e9da32da8a9d4
KrystianPi <38494173+KrystianPi@users.noreply.github.com>
2021-01-14 14:01:11 +0100

    Update zad4.hpp

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -1,5 +1,19 @@
 #include "zad1.hpp"
 
 #include <vector>
+double obliczMake(const std::vector<Tagliatelle>& v) {
+    double M;
+    M = v[v.size() - 1].ileMaki(1) + v[v.size() - 2].ileMaki(2) + v[v.size() - 3].ileMaki(3);
+    if (M > 100) {
+        throw 1;
+    }
+    else if (M > 50 && M <= 100) {
+        throw 2.;
+    }
+    else {
+        return M;
+    }
 
+
+}
 // tutaj funkcja obliczMake
